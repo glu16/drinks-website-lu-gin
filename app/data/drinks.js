80a1d4c17cd17690e49bfeb7a1587cbb8e78e70d
Gin Lu <glu23@my.bcit.ca>
2023-03-10 11:43:41 -0800

    Added in my files for Assignment 5

---
+++ app/data/drinks.js
@@ -0,0 +1,37 @@
+[
+  {
+    "title": "Peach Mint Frosé",
+    "description": "Rosé wine, frozen peaches, honey, mint leaves, fresh peach slices.",
+    "prep time": "5 mins",
+    "source": "Short Girl Tall Order",
+    "link": "https://shortgirltallorder.com/peach-mint-frose#recipe"
+  },
+  {
+    "title": "Lavendar Lemon Drop",
+    "description": "Vodka, lemon juice, lavendar simple syrup, dried lavendar.",
+    "prep time": "5 mins",
+    "source": "Short Girl Tall Order",
+    "link": "https://shortgirltallorder.com/lavender-lemon-martini#recipe"
+  },
+  {
+    "title": "Tiffany Mimosa",
+    "description": "Champagne, blue curacao, lemonade, corn syrup, sugar sprinkles.",
+    "prep time": "5 mins",
+    "source": "The Blond Cook",
+    "link": "https://theblondcook.com/tiffany-mimosas/"
+  },
+  {
+    "title": "Strawberry Mojito",
+    "description": "White rum, lime juice, sparkling water, strawberry simple syrup.",
+    "prep time": "5 mins",
+    "source": "The Blond Cook",
+    "link": "https://theblondcook.com/strawberry-mojitos/"
+  },
+  {
+    "title": "Pineapple Moscow Mule",
+    "description": "Vodka, pineapple juice, ginger beer, lime juice, fresh pineapple wedges.",
+    "prep time": "5 mins",
+    "source": "The Blond Cook",
+    "link": "https://theblondcook.com/pineapple-moscow-mule/"
+  }
+]
---
+++ node.js
@@ -0,0 +1,63 @@
+// REQUIRES
+const express = require("express");
+const app = express();
+app.use(express.json());
+const fs = require("fs");
+
+app.use("/js", express.static("./public/js"));
+app.use("/css", express.static("./public/css"));
+app.use("/img", express.static("./public/img"));
+
+// CALL BACK FUNCTIONS
+app.get("/", function (req, res) {
+    let doc = fs.readFileSync("./app/html/main.html", "utf8");
+    res.send(doc);
+});
+
+app.get("/main.html", function (req, res) {
+    let doc = fs.readFileSync("./app/html/main.html", "utf8");
+    res.send(doc);
+});
+
+app.get("/about.html", function (req, res) {
+    let doc = fs.readFileSync("./app/html/about.html", "utf8");
+    res.send(doc);
+}); 
+
+app.get("/table.html", function (req, res) {
+    let doc = fs.readFileSync("./app/html/table.html", "utf8");
+    res.send(doc);
+}); 
+
+app.get("/drinks", function (req, res) {
+
+    let formatOfResponse = req.query["format"];
+
+    if (formatOfResponse == "html") {
+        // MIME type
+        res.setHeader("Content-Type", "text/html");
+        res.send(fs.readFileSync("./app/data/drinks.html", "utf8"));
+
+    } else if (formatOfResponse == "json") {
+        // MIME type
+        res.setHeader("Content-Type", "application/json");
+        res.send(fs.readFileSync("./app/data/drinks.js", "utf8"));
+
+    } else {
+        // Just send JSON message
+        res.send({ status: "fail", msg: "Wrong format!" });
+    }
+});
+
+// PAGE NOT FOUND ERROR
+app.use(function (req, res, next) {
+    res.status(404).send("<html><head><title>Page not found!</title></head><body><p>Nothing here.</p></body></html>");
+});
+
+// RUN SERVER
+let port = 8000;
+app.listen(port, function () {
+    console.log("Assignment 5 listening on port " + port + "!");
+});
+
+
